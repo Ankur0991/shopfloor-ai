ddabe39391540b006fa9b76ba6dbef2dfc128ea9
Ank0991 <ankur.jain0991@gmail.com>
2026-08-30 22:18:03 +0200

    add baseline migration

--- app/models.py
+++ app/models.py
@@ -31,4 +31,3 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    
